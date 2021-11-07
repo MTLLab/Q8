5944df722cfcba85d00840c88b25e62848e4a1ee
KidLab2021 <88603169+KidLab2021@users.noreply.github.com>
2021-11-07 08:31:39 -0500

    Add unit test for get average

---
+++ ResponseTimeStatistics.UnitTest/StatsCollector_getAverage.cs
@@ -0,0 +1,84 @@
+using NUnit.Framework;
+
+namespace ResponseTimeStatistics.UnitTest
+{
+    public class StatsCollector_getAverage
+    {
+
+        private StatsCollector statsCollector;
+
+        [SetUp]
+        public void Setup()
+        {
+            statsCollector = new StatsCollector();
+        }
+
+        [Test]
+        public void getAverage_ZeroInput_ReturnZero()
+        {
+            Assert.AreEqual(statsCollector.getAverage(), 0);
+        }
+
+        [Test]
+        public void getAverage_OneInput_ReturnTheExistOne()
+        {
+            statsCollector.pushValue(25);
+            Assert.AreEqual(statsCollector.getAverage(), 25);
+        }
+
+        [Test]
+        public void getAverage_MultipleInput_ReturnTheAverage()
+        {
+            //1, 3, 9, 31, 48, 2000, 14567
+            statsCollector.pushValue(3);
+            statsCollector.pushValue(9);
+            statsCollector.pushValue(1);
+            statsCollector.pushValue(48);
+            statsCollector.pushValue(14567);
+            statsCollector.pushValue(31);
+            statsCollector.pushValue(1994);
+            Assert.AreEqual(statsCollector.getAverage(), 2379);
+        }
+
+        [Test]
+        public void getAverage_MultipleTheSameInput_ReturnTheAverage()
+        {
+            //1, 3, 9, 31, 48, 2000, 14567
+            statsCollector.pushValue(3);
+            statsCollector.pushValue(3);
+            statsCollector.pushValue(3);
+            statsCollector.pushValue(3);
+            Assert.AreEqual(statsCollector.getAverage(), 3);
+        }
+
+        [Test]
+        public void getAverage_MultipleInput_IgnoreNegative_ReturnTheAverage()
+        {
+            //-12323, 1, 3, 9, 31, 48, 2000, 14567
+            statsCollector.pushValue(3);
+            statsCollector.pushValue(9);
+            statsCollector.pushValue(1);
+            statsCollector.pushValue(48);
+            statsCollector.pushValue(14567);
+            statsCollector.pushValue(31);
+            statsCollector.pushValue(1994);
+            statsCollector.pushValue(-12323);
+            Assert.AreEqual(statsCollector.getAverage(), 2379);
+        }
+
+        [Test]
+        public void getAverage_MultipleInput_IgnoreBiggerThan19000_ReturnTheAverage()
+        {
+            //1, 3, 9, 31, 48, 2000, 14567, 19001
+            statsCollector.pushValue(3);
+            statsCollector.pushValue(9);
+            statsCollector.pushValue(1);
+            statsCollector.pushValue(48);
+            statsCollector.pushValue(14567);
+            statsCollector.pushValue(31);
+            statsCollector.pushValue(1994);
+            statsCollector.pushValue(19001);
+            Assert.AreEqual(statsCollector.getAverage(), 2379);
+        }
+    }
+}
